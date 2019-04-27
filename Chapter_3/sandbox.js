82c4ea41ca14cfea841e3ae8ef4e5e2a42b79550
Aaron <atsand23@gmail.com>
2019-04-27 16:44:05 -0400

    Do While Loops

--- Chapter_3/sandbox.js
+++ Chapter_3/sandbox.js
@@ -1,27 +1,36 @@
-//------------ while loops start------------------
-const names = ['shaun', 'mario', 'luigi'];
+// //------------ do while loops start------------------
+// let i = 0;
+// do{
+//     console.log('val of i is: ', i);
+//     i++;
+// } while(i < 5)
+
+// //------------ do while loops end------------------
+
+// //------------ while loops start------------------
+// //const names = ['shaun', 'mario', 'luigi'];
+
+// // let i = 0;
+// // while(i<5){
+// //     console.log('in loop', i);
+// //     i++;
+// // }
 
 // let i = 0;
-// while(i<5){
-//     console.log('in loop', i);
+// while(i < names.length){
+//     console.log(names[i]);
 //     i++;
 // }
 
-let i = 0;
-while(i < names.length){
-    console.log(names[i]);
-    i++;
-}
+// //------------ while loops end------------------
 
-//------------ while loops end------------------
+// //------------ for loops start------------------
 
-//------------ for loops start------------------
+// // for(let i = 0; i < 5; i++){
+// //     console.log('inside loop: ', i);
+// // }
 
-// for(let i = 0; i < 5; i++){
-//     console.log('inside loop: ', i);
-// }
-
-// console.log('loop finished');
+// // console.log('loop finished');
 
 // const names = ['shaun', 'mario', 'luigi'];
 
@@ -33,4 +42,4 @@ while(i < names.length){
 
 // console.log('loop finihed');
 
-//------------ for loops end------------------
+// //------------ for loops end------------------
