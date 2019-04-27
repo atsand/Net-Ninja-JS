7e5eb0542e13b991c0c2fe19ed792279c871b4c9
Aaron <atsand23@gmail.com>
2019-04-27 16:40:07 -0400

    While Loops

--- Chapter_3/sandbox.js
+++ Chapter_3/sandbox.js
@@ -1,4 +1,21 @@
-// for loops
+//------------ while loops start------------------
+const names = ['shaun', 'mario', 'luigi'];
+
+// let i = 0;
+// while(i<5){
+//     console.log('in loop', i);
+//     i++;
+// }
+
+let i = 0;
+while(i < names.length){
+    console.log(names[i]);
+    i++;
+}
+
+//------------ while loops end------------------
+
+//------------ for loops start------------------
 
 // for(let i = 0; i < 5; i++){
 //     console.log('inside loop: ', i);
@@ -6,12 +23,14 @@
 
 // console.log('loop finished');
 
-const names = ['shaun', 'mario', 'luigi'];
+// const names = ['shaun', 'mario', 'luigi'];
 
-for(let i = 0; i < names.length; i++){
-    // console.log(names[i]);
-    let html = `<div>${names[i]}</div>`;
-    console.log(html);
-}
+// for(let i = 0; i < names.length; i++){
+//     // console.log(names[i]);
+//     let html = `<div>${names[i]}</div>`;
+//     console.log(html);
+// }
 
-console.log('loop finihed');
+// console.log('loop finihed');
+
+//------------ for loops end------------------
